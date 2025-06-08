9fe5a0bc1b72faae65f54079c7176e0d0399a412
Bhavin-Pathak <bhavinpathak29@gmail.com>
2025-06-08 19:12:41 +0530

    feat: add animation delays, durations, and bubble configuration constants

---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+const ANIMATION_DELAYS = [
+  "0s",
+  "1s",
+  "2s",
+  "0.5s",
+  "1.5s",
+  "2.5s",
+  "1.8s",
+  "0.8s",
+];
+const ANIMATION_DURATIONS = [
+  "3s",
+  "4s",
+  "3.5s",
+  "2.8s",
+  "3.2s",
+  "4.2s",
+  "3.8s",
+  "2.5s",
+];
+
+const BUBBLE_CONFIG = [
+  { position: "top-4 left-6", size: "w-3 h-3", color: "bg-white/20" },
+  { position: "top-12 right-8", size: "w-2 h-2", color: "bg-blue-300/30" },
+  { position: "bottom-16 left-4", size: "w-4 h-4", color: "bg-purple-300/25" },
+  { position: "bottom-8 right-6", size: "w-2.5 h-2.5", color: "bg-white/15" },
+  {
+    position: "top-1/2 left-8",
+    size: "w-1.5 h-1.5",
+    color: "bg-indigo-300/40",
+  },
+  {
+    position: "top-1/3 right-12",
+    size: "w-3.5 h-3.5",
+    color: "bg-blue-200/20",
+  },
+  {
+    position: "bottom-1/3 left-12",
+    size: "w-2 h-2",
+    color: "bg-purple-200/30",
+  },
+  { position: "top-20 left-1/2", size: "w-1 h-1", color: "bg-white/25" },
+];
+
+export  { BUBBLE_CONFIG, ANIMATION_DELAYS, ANIMATION_DURATIONS };
