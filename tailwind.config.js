7cae5d93a565713cb27f27129046fa6ac7a6e35e
Bhavin-Pathak <bhavinpathak29@gmail.com>
2025-06-08 17:45:39 +0530

    fix: update tailwind.config.js to include content paths for React

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React ke liye yeh zaroori hai
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
